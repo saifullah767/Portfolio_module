be0b92e4e9f76b5c8a011d2e67eb4d979e32c5f2
skj99667788@gmail.com <skj99667788@gmail.com>
2022-04-05 03:21:04 +0500

    index.js & Toggle button functionality add

---
+++ index.js
@@ -0,0 +1,8 @@
+function menu_btn() {
+  let a = document.getElementById("myLinks");
+  if (a.style.display === "none") {
+    a.style.display = "block";
+  } else {
+    a.style.display = "none";
+  }
+}
