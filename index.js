48829e6ca9b05a7d61760eed15dec96e0e128a91
skj99667788@gmail.com <skj99667788@gmail.com>
2022-04-06 03:41:56 +0500

    html , css and js code set

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+let body = document.body;
+let b = document.getElementById("pageone");
+let c = document.getElementById("blur");
 function menu_btn() {
   let a = document.getElementById("myLinks");
-  let b = document.getElementById("pageone");
-  let c = document.getElementById("blur");
   if (a.style.display === "none") {
     a.style.display = "block";
+    body.style.overflowY = "hidden"
     b.style.filter = "blur(2px)";
     c.style.filter = "blur(2px)";
+
   } else {
     a.style.display = "none";
     b.style.filter = "none";
     c.style.filter = "none";
+    body.style.overflowY = "scroll"
+  }
+}
+function popup(){
+  const project = document.getElementById("popup");
+
+  if (project.style.display === "none") {
+    project.style.display = "flex";
+    project.style.overflowY = "scroll";
+    b.style.filter = "blur(2px)";
+    c.style.filter = "blur(2px)";
+    body.style.overflowY = "hidden"
+
+
+  } else {
+    project.style.display = "none";
+    b.style.filter = "none";
+    c.style.filter = "none";
+    body.style.overflowY = "scroll"
   }
 }
