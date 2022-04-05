f2a3744dee35616f2ca1a427c70f30377bf051d4
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-04-05 17:57:36 -0500

    JS button functionality

--- index.js
+++ index.js
@@ -24,13 +24,130 @@ function popup(){
     project.style.overflowY = "scroll";
     b.style.filter = "blur(2px)";
     c.style.filter = "blur(2px)";
-    body.style.overflowY = "hidden"
-
-
+    body.style.overflowY = "hidden";
   } else {
     project.style.display = "none";
     b.style.filter = "none";
     c.style.filter = "none";
-    body.style.overflowY = "scroll"
+    body.style.overflowY = "scroll";
   }
+
+  /*
+
+const proyecto1 = {
+  name: "Multi-Post Stories",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  featuredImage: "image.jpg",
+  technologies: ["html", "Bootstrap", "Ruby on Rails"],
+  linkToLive:
+    "https://alexpuente0https://github.com/alexpuente0/My-Portfolio/blob/main/README.md.github.io/My-Portfolio/",
+  linkToSource: "https://github.com/alexpuente0",
+};
+
+const proyecto2 = {
+  name: "Professional Art Printing Data",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  featuredImage: "image.jpg",
+  technologies: ["html", "Bootstrap", "Ruby on Rails"],
+  linkToLive: "https://github.com/alexpuente0/My-Portfolio/blob/main/README.md",
+  linkToSource: "https://github.com/alexpuente0",
+};
+
+const proyecto3 = {
+  name: "Professional Art Printing Data",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  featuredImage: "image.jpg",
+  technologies: ["html", "Bootstrap", "Ruby on Rails"],
+  linkToLive: "https://github.com/alexpuente0/My-Portfolio/blob/main/README.md",
+  linkToSource: "https://github.com/alexpuente0",
+};
+
+const proyecto4 = {
+  name: "Professional Art Printing Data",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  featuredImage: "image.jpg",
+  technologies: ["html", "Bootstrap", "Ruby on Rails"],
+  linkToLive: "https://github.com/alexpuente0/My-Portfolio/blob/main/README.md",
+  linkToSource: "https://github.com/alexpuente0",
+};
+
+const proyecto5 = {
+  name: "Professional Art Printing Data",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  featuredImage: "image.jpg",
+  technologies: ["html", "Bootstrap", "Ruby on Rails"],
+  linkToLive: "https://github.com/alexpuente0/My-Portfolio/blob/main/README.md",
+  linkToSource: "https://github.com/alexpuente0",
+};
+
+const proyecto6 = {
+  name: "Professional Art Printing Data",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  featuredImage: "image.jpg",
+  technologies: ["html", "Bootstrap", "Ruby on Rails"],
+  linkToLive: "https://github.com/alexpuente0/My-Portfolio/blob/main/README.md",
+  linkToSource: "https://github.com/alexpuente0",
+};
+
+const proyecto7 = {
+  name: "Professional Art Printing Data",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  featuredImage: "image.jpg",
+  technologies: ["html", "Bootstrap", "Ruby on Rails"],
+  linkToLive: "https://github.com/alexpuente0/My-Portfolio/blob/main/README.md",
+  linkToSource: "https://github.com/alexpuente0",
+}; 
+
+let allProjects = [proyecto1, proyecto2, proyecto3, proyecto4, proyecto5, proyecto6, proyecto7]
+
+function togglePopup() {
+document.getElementById("popup-1").classList.toggle("active");
+
+const title = document.querySelector(".projectname")
+title.innerHTML = `
+ <h3>${proyecto1.name}</h3>
+`;
+
+const theList = document.querySelector(".ulist");
+theList.innerHTML = `
+               <ul class="poplist">
+                <li class="popitem">${proyecto1.technologies[0]}</li>
+                <li class="popitem">${proyecto1.technologies[1]}</li>
+                <li class="popitem">${proyecto1.technologies[2]}</li>
+              </ul>
+`;
+
+const theImage = document.querySelector(".imagesub");
+theImage.innerHTML = `
+ <img class="popimage" src="./icons/popsnapshot.png" alt="default image of the project">
+`;
+
+const theText = document.querySelector(".projectext");
+theText.innerHTML = `
+ <p>
+  ${proyecto1.description}
+ </p>;
+ `
+const launch = document.querySelector(".gotoproject")
+launch.innerHTML = `
+ <a href=${proyecto1.linkToLive}>
+    <button class="btstyle pjbutton" type="button"><span>See Live <img src="./icons/export.png" alt="launch project"></span></button>
+    </a>
+  <a href=${proyecto1.linkToSource}>
+ <button class="btstyle pjbutton" type="button"><span>See Source <img src="./icons/github-w.png" alt="go to repo"></span></button>
+    </a>
+`;
+
+} 
+
+
+
+  */
 }
