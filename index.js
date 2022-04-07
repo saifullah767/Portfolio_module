c02fa14269be67c7ebeda09175935c619dfd014b
skj99667788@gmail.com <skj99667788@gmail.com>
2022-04-08 01:37:08 +0500

    linters error solve 1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let body = document.body;
 let b = document.getElementById("pageone");
 let c = document.getElementById("blur");
@@ -14,90 +13,122 @@ function menu_btn() {
     b.style.filter = "none";
     c.style.filter = "none";
     body.style.overflowY = "scroll";
-  }}
+  }
+}
 
-
-
-  let data = {
-    projects: [ {
+let data = {
+  projects: [
+    {
       key: 0,
-  name: "Tonic",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    normaldesc: "A daily selection of privately personalized reads; no accounts or sign-ups required",
-    featuredImage: "./Portoflio/Snapshoot1.png",
-  MobileImage: "./images/Snap.png",
-  technologies: ["html", "CSS", "Javascript", "Github", "Ruby", "Bootstrap"],
-  company: "CANOPY",
-  position: "BACK END DEV",
-  year: "2015",
-  dot: "./images/Counter.png",
-  closebtn: "./images/closeIcon.png",
-  linkToLive:
-    "https://saifullah767.github.io/Portfolio_module/project_one.html",
-  linkToSource: "https://github.com/saifullah767/Portfolio_module.git",
-},
- {
-  key: 1,
-  name: "Multi-Post Stories",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    normaldesc:"A daily selection of privately personalized reads; no accounts or sign-ups required",
-    featuredImage: "./Portoflio/Snapshoot4.png",
-  MobileImage: "./images/Snap2.png",
-  company: "CANOPY",
-  position: "BACK END DEV",
-  year: "2015",
-  dot: "./images/Counter.png",
-  technologies: ["html", "CSS", "Javascript", "Github", "Ruby", "Bootstrap"],
-  closebtn: "./images/closeIcon.png",
-  linkToLive:
-  "https://saifullah767.github.io/Portfolio_module/project_one.html",
-  linkToSource: "https://github.com/saifullah767/Portfolio_module.git",
-},
- {
-  key: 3,
-  name: "Tonic",
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    normaldesc:"A daily selection of privately personalized reads; no accounts or sign-ups required",
-    featuredImage: "./Portoflio/Snapshoot3.png",
-  MobileImage: "./images/Snap3.png",
-  company: "CANOPY",
-  position: "BACK END DEV",
-  year: "2015",
-  dot: "./images/Counter.png",
-  technologies: ["html", "CSS", "Javascript", "Github", "Ruby", "Bootstrap"],
-  closebtn: "./images/closeIcon.png",
-  linkToLive:
-    "https://saifullah767.github.io/Portfolio_module/project_one.html",
-    linkToSource: "https://github.com/saifullah767/Portfolio_module.git",
-  },
-{
-  key: 4,
-  name: "Multi-Post Stories",
-  description:
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-  normaldesc:"A daily selection of privately personalized reads; no accounts or sign-ups required",
-  featuredImage: "./Portoflio/Snapshoot2.png",
-  MobileImage: "./images/Snap4.png",
-  company: "CANOPY",
-  position: "BACK END DEV",
-  year: "2015",
-  dot: "./images/Counter.png",
-  technologies: ["html", "CSS", "Javascript", "Github", "Ruby", "Bootstrap"],
-  closebtn: "./images/closeIcon.png",
-  linkToLive:
-  "https://saifullah767.github.io/Portfolio_module/project_one.html",
-  linkToSource: "https://github.com/saifullah767/Portfolio_module.git",
-},
-],
+      name: "Tonic",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      normaldesc:
+        "A daily selection of privately personalized reads; no accounts or sign-ups required",
+      featuredImage: "./Portoflio/Snapshoot1.png",
+      MobileImage: "./images/Snap.png",
+      technologies: [
+        "html",
+        "CSS",
+        "Javascript",
+        "Github",
+        "Ruby",
+        "Bootstrap",
+      ],
+      company: "CANOPY",
+      position: "BACK END DEV",
+      year: "2015",
+      dot: "./images/Counter.png",
+      closebtn: "./images/closeIcon.png",
+      linkToLive:
+        "https://saifullah767.github.io/Portfolio_module/project_one.html",
+      linkToSource: "https://github.com/saifullah767/Portfolio_module.git",
+    },
+    {
+      key: 1,
+      name: "Multi-Post Stories",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      normaldesc:
+        "A daily selection of privately personalized reads; no accounts or sign-ups required",
+      featuredImage: "./Portoflio/Snapshoot4.png",
+      MobileImage: "./images/Snap2.png",
+      company: "CANOPY",
+      position: "BACK END DEV",
+      year: "2015",
+      dot: "./images/Counter.png",
+      technologies: [
+        "html",
+        "CSS",
+        "Javascript",
+        "Github",
+        "Ruby",
+        "Bootstrap",
+      ],
+      closebtn: "./images/closeIcon.png",
+      linkToLive:
+        "https://saifullah767.github.io/Portfolio_module/project_one.html",
+      linkToSource: "https://github.com/saifullah767/Portfolio_module.git",
+    },
+    {
+      key: 3,
+      name: "Tonic",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      normaldesc:
+        "A daily selection of privately personalized reads; no accounts or sign-ups required",
+      featuredImage: "./Portoflio/Snapshoot3.png",
+      MobileImage: "./images/Snap3.png",
+      company: "CANOPY",
+      position: "BACK END DEV",
+      year: "2015",
+      dot: "./images/Counter.png",
+      technologies: [
+        "html",
+        "CSS",
+        "Javascript",
+        "Github",
+        "Ruby",
+        "Bootstrap",
+      ],
+      closebtn: "./images/closeIcon.png",
+      linkToLive:
+        "https://saifullah767.github.io/Portfolio_module/project_one.html",
+      linkToSource: "https://github.com/saifullah767/Portfolio_module.git",
+    },
+    {
+      key: 4,
+      name: "Multi-Post Stories",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      normaldesc:
+        "A daily selection of privately personalized reads; no accounts or sign-ups required",
+      featuredImage: "./Portoflio/Snapshoot2.png",
+      MobileImage: "./images/Snap4.png",
+      company: "CANOPY",
+      position: "BACK END DEV",
+      year: "2015",
+      dot: "./images/Counter.png",
+      technologies: [
+        "html",
+        "CSS",
+        "Javascript",
+        "Github",
+        "Ruby",
+        "Bootstrap",
+      ],
+      closebtn: "./images/closeIcon.png",
+      linkToLive:
+        "https://saifullah767.github.io/Portfolio_module/project_one.html",
+      linkToSource: "https://github.com/saifullah767/Portfolio_module.git",
+    },
+  ],
 };
 let myProjects = data.projects;
-const Work = document.querySelector('#portfolio');
-function implementProject(){
-  let i=0 ;
-  myProjects.forEach((project) =>  {
+const Work = document.querySelector("#portfolio");
+function implementProject() {
+  let i = 0;
+  myProjects.forEach((project) => {
     const cardId = `card-button-${i + 1}`;
     Work.innerHTML += ` 
       <div class="card">
@@ -132,45 +163,45 @@ function implementProject(){
     </section>
   </div>
   <br/>
-` 
-    i += 1; 
-});
+`;
+    i += 1;
+  });
 }
 implementProject();
 
-let popProjects =data.projects[0];
-function popup_project(){
-const project = document.getElementById("popup");
-    if (project.style.display === "none") {
-      project.style.display = "flex";
-      project.style.overflowY = "scroll";
-      b.style.filter = "blur(2px)";
-      c.style.filter = "blur(2px)";
-      body.style.overflowY = "hidden";
-  
-      let title = document.querySelector(".title-one");
-      title.innerHTML = `<h2>${popProjects.name}</h2>`;
-  
-      const canopy = document.querySelector(".canopy");
-      canopy.innerHTML = `           
+let popProjects = data.projects[0];
+function popup_project() {
+  const project = document.getElementById("popup");
+  if (project.style.display === "none") {
+    project.style.display = "flex";
+    project.style.overflowY = "scroll";
+    b.style.filter = "blur(2px)";
+    c.style.filter = "blur(2px)";
+    body.style.overflowY = "hidden";
+
+    let title = document.querySelector(".title-one");
+    title.innerHTML = `<h2>${popProjects.name}</h2>`;
+
+    const canopy = document.querySelector(".canopy");
+    canopy.innerHTML = `           
    <span class="can-pop"> CANOPY </span>
   <img class="dot" src="./images/Counter.png" alt="" />
   <span class="side-pop"> BACK END DEV </span>
   <img class="dot" src="./images/Counter.png" alt="" />
   <span class="side-pop"> 2015 </span>
   `;
-  
-      const theImage = document.querySelector(".pop-websnap");
-      theImage.innerHTML = `
+
+    const theImage = document.querySelector(".pop-websnap");
+    theImage.innerHTML = `
   <img  src="${popProjects.featuredImage}" alt="Project screenshot" />
   `;
-  
-      const paratext = document.querySelector(".pop-text");
-      paratext.innerHTML = `
+
+    const paratext = document.querySelector(".pop-text");
+    paratext.innerHTML = `
   <p> ${popProjects.description} </p>`;
-  
-      const theList = document.querySelector(".links-pop");
-      theList.innerHTML = `
+
+    const theList = document.querySelector(".links-pop");
+    theList.innerHTML = `
   <ul>
   <li class='link'>${popProjects.technologies[0]}</li>
   <li class='link'>${popProjects.technologies[1]}</li>
@@ -186,14 +217,10 @@ const project = document.getElementById("popup");
   <a href="${popProjects.linkToLive}" class="pro-pop focusable"> See Live <img class="btn_icon" src="./images/share.png" alt="icon"> </a>
   <a href="${popProjects.linkToSource}" class="pro-pop focusable"> See Source <img class="btn_icon" src="./images/github_purple.png" alt="icon"> </a>
   `;
-    } else {
-      project.style.display = "none";
-      b.style.filter = "none";
-      c.style.filter = "none";
-      body.style.overflowY = "scroll";
-    }
- 
-  
+  } else {
+    project.style.display = "none";
+    b.style.filter = "none";
+    c.style.filter = "none";
+    body.style.overflowY = "scroll";
+  }
 }
-
-
